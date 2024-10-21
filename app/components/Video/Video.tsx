d9eb17c6e4d43811749321b44ee9e2194ed3d3d9
Romainbocquet <94678672+Romainbocquet@users.noreply.github.com>
2024-10-21 19:44:22 +0200

    picto sound on off

--- app/components/Video/Video.tsx
+++ app/components/Video/Video.tsx
@@ -11,36 +11,73 @@ export default function Video() {
     if (videoRef.current) {
       videoRef.current.muted = !videoRef.current.muted;
       setMuted(videoRef.current.muted);
+      console.log('patate');
+      
     }
   };
 
   return (
     <div className="relative w-full px-10">
-          <button
-            onClick={toggleSound}
-            className="absolute top-2 left-2 z-10 bg-gray-700 text-white p-2"
+      <button
+        onClick={toggleSound}
+        className={`absolute top-2 bg-gray-700 text-white p-2 z-50 ${styles.buttonSound}`}
+      >
+        {muted ? (
+          <svg
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {muted ? "Activer le son" : "Désactiver le son"}
-          </button>
-
-          <video
-            ref={videoRef}
-            className="w-full h-auto"
-            muted
-            autoPlay
-            loop
-            preload="auto"
-          >
-            <source src="/video/video-header.mp4" type="video/mp4" />
-            <track
-              src="/path/to/captions.vtt"
-              kind="subtitles"
-              srcLang="en"
-              label="English"
+            <path
+              d="M19 6C20.5 7.5 21 10 21 12C21 14 20.5 16.5 19 18M16 8.99998C16.5 9.49998 17 10.5 17 12C17 13.5 16.5 14.5 16 15M3 10.5V13.5C3 14.6046 3.5 15.5 5.5 16C7.5 16.5 9 21 12 21C14 21 14 3 12 3C9 3 7.5 7.5 5.5 8C3.5 8.5 3 9.39543 3 10.5Z"
+              stroke="#fff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
-            Your browser does not support the video tag.
-          </video>
-          <h1 className={`absolute ${styles.videoTitle}`}>L’EXCELLENCE DU LASER</h1>
-        </div>
+          </svg>
+        ) : (
+          <svg
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 8.14307C3.4148 8.66137 3 9.49393 3 10.5V13.5C3 14.6046 3.5 15.5 5.5 16C7.5 16.5 9 21 12 21C12.6098 21 13.0337 19.3265 13.2717 17M3 3L21 21M9 4.60756C9.84604 3.71548 10.8038 3 12 3C12.7739 3 13.2484 5.69533 13.4233 9"
+              stroke="#fff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        )}
+      </button>
+
+      <video
+        ref={videoRef}
+        className="w-full h-auto"
+        muted={muted}
+        autoPlay
+        loop
+        preload="auto"
+      >
+        <source src="/video/video-header.mp4" type="video/mp4" />
+        <track
+          src="/path/to/captions.vtt"
+          kind="subtitles"
+          srcLang="en"
+          label="English"
+        />
+        Your browser does not support the video tag.
+      </video>
+
+      <h1 className={`absolute ${styles.videoTitle}`}>
+        L’EXCELLENCE DU LASER
+      </h1>
+    </div>
   );
 }
